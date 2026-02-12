fca04b2cd7ae0d1af323855a2d99bfa7f338143f
Siret Nathan <siretnathan@MacBookPro.lan>
2026-02-12 11:35:14 +0100

    Ajout de Spring Security

--- src/main/java/com/p3springboot/configuration/SpringSecurityConfig.java
+++ src/main/java/com/p3springboot/configuration/SpringSecurityConfig.java
@@ -1,28 +1,68 @@
-/*package com.p3springboot.configuration;
+package com.p3springboot.configuration;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
 
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.build();
-    }   
-}
+    private final String jwtKey = "VeUA3lad7H7UHZEbh1Wo0WQMMcLIyTyo";
 
-/*
-Cette méthode renvoie les éléments suivants : 
-Cache-Control: no-cache, no-store, max-age=0, must-revalidate
-Pragma: no-cache
-Expires: 0
-X-Content-Type-Options: nosniff
-Strict-Transport-Security: max-age=31536000 ; includeSubDomains
-X-Frame-Options: DENY
-X-XSS-Protection: 1; mode=block
-*/
+    //Chaine de filtre de sécurité
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
+    }
+    //Encryption du mot de passe
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Méthode qui permet de tester des utilisateurs 
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                               .username("user")
+                               .password(passwordEncoder()
+                               .encode("Password"))
+                               .roles("USER")
+                               .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+}
